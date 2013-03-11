92b178ae8fb2da1c5e49e716222946371c4b9bb7
Mika Attila <radiantstatue@gmail.com>
2013-03-11 10:38:26 +0100

    Add Clock class

---
+++ Clock.hpp
@@ -0,0 +1,26 @@
+#ifndef CLOCK_HPP
+#define CLOCK_HPP
+
+#include <SDL.h>
+
+class Clock
+{
+public:
+    Clock() :
+        start(SDL_GetTicks()) {}
+
+    void Restart()
+    {
+        start = SDL_GetTicks();
+    }
+
+    Uint32 Ticks()
+    {
+        return SDL_GetTicks() - start;
+    }
+
+private:
+    Uint32 start;
+};
+
+#endif // CLOCK_HPP
