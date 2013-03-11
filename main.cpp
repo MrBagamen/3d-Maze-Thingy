572d50f4a2a61b8ba09e8a8cf5ff71e319420ecf
MrBagamen <mrbagamen@gmail.com>
2013-03-11 22:49:21 +0100

    niggin collision

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ int main (int argc, char** argv)
     floor.LoadTexture("res/floor.png", conf.GetBool("anisotropic"), conf.GetFloat("afLevel"), 50.0f, 50.0f);
 
     //Test/Wall
-    Wall w(-10.0f, 0.0f, -30.0f,-50.0f,20.0f, 0.0f);
+    Wall w(-10.0f, 0.0f, -30.0f, 50.0f,20.0f, 0.0f);
     //Wall w(-10.0f, 0.0f, -30.0f, 0.0f,20.0f, 50.0f);
     w.LoadTexture("res/wall.png", conf.GetBool("anisotropic"), conf.GetFloat("afLevel"), 5.0f, 2.0f);
 
